461d38a68566d6f56e9fe3f58ded51952ec55a44
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-05-01 13:02:24 +0500

    commit

--- src/context/orderFoods.jsx
+++ src/context/orderFoods.jsx
@@ -3,14 +3,13 @@ import { createContext } from "react";
 
 const Context = createContext();
 
-function Provider ({children}) {
-    const [orderFoods, setOrderFoods] = useState([])
-    return(
-        <Context.Provider 
-        value={{orderFoods, setOrderFoods}}>
-             {children}
-        </Context.Provider>
-    )
+function Provider({ children }) {
+  const [orderFoods, setOrderFoods] = useState([]);
+  return (
+    <Context.Provider value={{ orderFoods, setOrderFoods }}>
+      {children}
+    </Context.Provider>
+  );
 }
 
-export {Context, Provider};
+export { Context, Provider };
