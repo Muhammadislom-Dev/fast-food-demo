65a7ad240ea99f8197b48a8a8679c388d137675d
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-05-01 14:53:54 +0500

    commit

--- src/Page/HomePage/HomePage.jsx
+++ src/Page/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import { Switch, Route } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import HomeContent from "../../components/HomeContent/HomeContent";
+import SwiperFood from "../../components/Swiper/Swiper";
 
 const Homepage = () => {
   return (
     <div className="homepage">
+      <SwiperFood />
       <Sidebar />
       <Switch>
         <Route path="/:dishtype">
